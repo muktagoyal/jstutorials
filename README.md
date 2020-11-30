53e71f97e521b476383090c791a349465880e923
Mukta <you@example.com>
2020-11-30 11:53:47 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# testmukta
+```
+npm -v
+mkdir projectname
+cd projectname
+npm init
+node index.js
+===================
+Open package.json file and put "start" : "node index.js" under script tag. 
+```
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+
+function getString(params:String) {
+    return "Hello" +  params;
+    
+}
+
+const getStr = (params:String)=>{
+    return "Hello"+ params;
+}
+
+console.log(getString("Mukta"));
+console.log(getStr("MuktaGoyal"));
